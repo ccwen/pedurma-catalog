b0b1bc6dbeb3def143cd790ffa3a534b46e9db23
yapcheahshen <yapcheahshen@gmail.com>
2014-07-16 12:07:08 +0800

    better naming

--- pagemap.js
+++ pagemap.js
@@ -5,32 +5,32 @@ var _H=["1b1","20b7","33b6","47a3","63a1","88b2","105a6","121b6","138a7",
 		"155b3","173a7","192a6","213b5","231a4","248b2","269b3","289b3","306b1",
 		"325a2","343a2","361a3"];
 
-var volpage2line=function(vp) {
+var from_vp=function(vp) { // pseudo line from volume-page
 	var parts=vp.split(/[ab]+/);
-	var pg=parseInt(parts[0]),  side=vp[ parts[0].length],  line=parseInt(parts[1] || "1")  - 1;
-	var out=pg * 2 * 8 + line;
+	var page=parseInt(parts[0]),  side=vp[ parts[0].length],  line=parseInt(parts[1] || "1")  - 1;
+	var out=page * 2 * 8 + line;
 	if ("b"==side) out+=8;
 	return out;
 }
-var line2volpage=function(absline) {
-	var line =( absline % 8 );
-	var page = Math.floor( absline / 8 ) ;
+var to_vp=function(pseudo_line) { // pseudo line to volume-page
+	var line =( pseudo_line % 8 );
+	var page = Math.floor( pseudo_line / 8 ) ;
 	var side = page % 2 ? "b":"a" ;
 	page = Math.floor( page / 2 );
 	return page+side+ (line+1);
 }
-var J=_J.map(function(a){ return volpage2line(a)});
-var H=_H.map(function(a){ return volpage2line(a)});
+var J=_J.map(function(a){ return from_vp(a)});
+var H=_H.map(function(a){ return from_vp(a)});
 
 var guess_page=function(vp,from,to) {
-	var line=volpage2line(vp);
-	var r=from.findIndex(function(e){ return e>line }) ;
+	var pseudo_line=from_vp(vp);
+	var r=from.findIndex(function(e){ return e>pseudo_line }) ;
 	if (r==0) return "**underflow**";
 	if (r==-1) return "**overflow**";
 
-	var ratio=(line-from[r-1]) / (from[r] - from[r-1]);
+	var ratio=(pseudo_line-from[r-1]) / (from[r] - from[r-1]);
 	var estimate=to[r-1]  +  ratio*(to[r] - to[r-1]);
-	return line2volpage(Math.floor(estimate));
+	return to_vp(Math.floor(estimate));
 }
 
 /* UI related code */
