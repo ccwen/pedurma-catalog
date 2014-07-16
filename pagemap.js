10e30e7620fde9c696eec479917b649e0c141611
yapcheahshen <yapcheahshen@gmail.com>
2014-07-16 15:42:17 +0800

    fix rounding problem, add param for lineperpage

--- pagemap.js
+++ pagemap.js
@@ -5,32 +5,32 @@ var _H=["1b1","20b7","33b6","47a3","63a1","88b2","105a6","121b6","138a7",
 		"155b3","173a7","192a6","213b5","231a4","248b2","269b3","289b3","306b1",
 		"325a2","343a2","361a3"];
 
-var from_vp=function(vp) { // pseudo line from volume-page
+var from_vp=function(vp, lpp) { // pseudo line from volume-page
 	var parts=vp.split(/[ab]+/);
-	var page=parseInt(parts[0]),  side=vp[ parts[0].length],  line=parseInt(parts[1] || "1")  - 1;
-	var out=page * 2 * 8 + line;
-	if ("b"==side) out+=8;
+	var npage=2* parseInt(parts[0]),  side=vp[parts[0].length],  line=parseInt(parts[1] || "1")  - 1;
+	var out=npage * lpp + line;
+	if ("b"==side) out+=lpp;
 	return out;
 }
-var to_vp=function(pseudo_line) { // pseudo line to volume-page
-	var line =( pseudo_line % 8 );
-	var page = Math.floor( pseudo_line / 8 ) ;
-	var side = page % 2 ? "b":"a" ;
-	page = Math.floor( page / 2 );
-	return page+side+ (line+1);
+var to_vp=function(estimate,lpp) { // pseudo line to volume-page
+	var vline=Math.round(estimate); 
+	var line =(vline % lpp );
+	var npage = Math.floor( vline / lpp );
+	var side = npage % 2 ? "b":"a" ;
+	return Math.floor( npage / 2 ) +side+ (line+1);
 }
-var J=_J.map(function(a){ return from_vp(a)});
-var H=_H.map(function(a){ return from_vp(a)});
+var J=_J.map(function(a){ return from_vp(a,8)}) ; J.linesPerPage=8;
+var H=_H.map(function(a){ return from_vp(a,7)}); H.linesPerPage=7;
 
 var guess_page=function(vp,from,to) {
-	var pseudo_line=from_vp(vp);
-	var r=from.findIndex(function(e){ return e>pseudo_line }) ;
-	if (r==0) return "**underflow**";
-	if (r==-1) return "**overflow**";
+	var vline=from_vp(vp,from.linesPerPage);
+	var i=from.findIndex(function(e){ return e>vline }) ;
+	if (i==0) return "underflow !";
+	if (i==-1) return "overflow !";
 
-	var ratio=(pseudo_line-from[r-1]) / (from[r] - from[r-1]);
-	var estimate=to[r-1]  +  ratio*(to[r] - to[r-1]);
-	return to_vp(Math.floor(estimate));
+	var ratio=(vline-from[i-1]) / (from[i] - from[i-1]);
+	var estimate=to[i-1]  +  ratio*(to[i] - to[i-1]);
+	return to_vp(estimate,to.linesPerPage);
 }
 
 /* UI related code */
